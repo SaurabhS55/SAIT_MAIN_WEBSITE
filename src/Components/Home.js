8f4ceedb22b2a5d5f68d766e5c26d9a166fd9d80
TANMAY SHINGDE <tanmayshingde8@gmail.com>
2024-01-01 21:30:11 +0530

    hero text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,19 @@ import "../Style/Home.css";
 
 function Home() {
   useEffect(() => {}, []);
+  const text1 = "Beyond The Limitations Next To The Perfection";
+
+  // <h1 className="overflow-hidden text-2xl font-bold leading-6 text-white">
+  //       {text.match(/./gu)!.map((char, index) => (
+  //         <span
+  //           className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+  //           key={`${char}-${index}`}
+  //           style={{ animationDelay: `${index * 0.05}s` }}
+  //         >
+  //           {char === " " ? "\u00A0" : char}
+  //         </span>
+  //       ))}
+  //     </h1>
 
   return (
     <div className="container w-11/12 rounded-3xl bg-gradient-to-b from-[#015DB9] to-[#022141] text-white">
@@ -18,13 +31,28 @@ function Home() {
               <br />
               Next To The <span className="text-pink-400">Perfection</span>
             </h1>
+            {/* <h1 className="overflow-hidden leading-6 text-white main-heading1 text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+              {Array(text1).map((char, index) => (
+                <span
+                  className={`main-heading2 animate-text-reveal inline-block [animation-fill-mode:backwards] ${
+                    index >= 11 && index <= 21 ? " text-pink-400" : " "
+                  }`}
+                  key={`${char}-${index}`}
+                  style={{ animationDelay: `${index * 0.05}s` }}
+                >
+                  {char === "x" ? "\n" : char === " " ? "\u00A0" : char}
+                </span>
+              ))}
+              <br></br>
+            </h1> */}
             {/* <h1 className="main-heading text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
               Beyond The <span className="magic">Limitations</span>
               <br />
               Next To The <span className="magic">Perfection</span>
             </h1> */}
             <p className="text-lg md:text-xl lg:text-2xl text-gray-100 mb-6">
-              Discover a world of possibilities and perfection with us. Join our community and explore together!
+              Discover a world of possibilities and perfection with us. Join our
+              community and explore together!
             </p>
             <div className="-ml-4 scale-90 md:scale-100 md:justify-normal flex space-x-6 mt-4 mb-3">
               <button className="bg-gray-400 w-16 h-16 flex justify-center items-center rounded-full bg-opacity-20 ">
@@ -40,7 +68,11 @@ function Home() {
           </div>
           <div className="md:ml-4 grid-item-2">
             <div className="">
-              <img src={SvgImage} alt="team-img" className=" drop-shadow-xl z-10" />
+              <img
+                src={SvgImage}
+                alt="team-img"
+                className=" drop-shadow-xl z-10"
+              />
             </div>
           </div>
         </div>
@@ -50,5 +82,3 @@ function Home() {
 }
 
 export default Home;
-
-
--- src/Components/ResponsiveGallery.js
+++ src/Components/ResponsiveGallery.js
@@ -1,51 +1,126 @@
+// import React, { useState, forwardRef, useContext, useEffect } from "react";
+// import imageContext from "../Contexts/imageContext";
+
+// const ResponsiveGallery = forwardRef((props, ref) => {
+//   const [selectedImage, setSelectedImage] = useState(null);
+//   const context = useContext(imageContext);
+//   const { images, getImages } = context;
+
+//   useEffect(() => {
+//     getImages();
+//     // eslint-disable-next-line
+//   }, []);
+
+//   const handleImageSelect = (image) => {
+//     setSelectedImage(image);
+//   };
+
+//   const formatDate = (dateString) => {
+//     const options = { year: "numeric", month: "long", day: "numeric" };
+//     return new Date(dateString).toLocaleDateString(undefined, options);
+//   };
+
+//   const recentImages = (images.length!==0)?images.filter((image)=>{
+//     if(image.identifier.indexOf("gallery")!==-1){
+//       return image;
+//     }else{
+//       return null;
+//     }
+//   }).slice(0,6):[];
+
+//   return (
+//     <div>
+//       {recentImages.length !== 0 ? (
+//         <div className="w-11/12 mx-auto mt-24 p-6" ref={ref}>
+//           <h2 className="colorfont text-3xl mb-6">Recent Gallery</h2>
+//           <div className="responsive-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+//             {recentImages.map((image) => (
+//               <div key={image._id} className="relative group overflow-hidden cursor-pointer hover:z-10">
+//                 <img
+//                   src={image.imageLink}
+//                   alt="Memory"
+//                   className="w-full h-72 object-cover transition-transform transform scale-100 group-hover:scale-110"
+//                   onClick={() => handleImageSelect(image)}
+//                 />
+//                 <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-60 transition-opacity duration-300 group-hover:opacity-0 flex items-center justify-center">
+//                   <p className="text-white text-sm">{formatDate(image.date)}</p>
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       ) : (
+//         <div></div>
+//       )}
+//     </div>
+//   );
+// });
+
+// export default ResponsiveGallery;
+
 import React, { useState, forwardRef, useContext, useEffect } from "react";
 import imageContext from "../Contexts/imageContext";
+import "../Style/Gallery.css";
 
 const ResponsiveGallery = forwardRef((props, ref) => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState({imageLink:""});
   const context = useContext(imageContext);
   const { images, getImages } = context;
-
-  useEffect(() => {
-    getImages();
-    // eslint-disable-next-line
-  }, []);
 
   const handleImageSelect = (image) => {
     setSelectedImage(image);
   };
 
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
-  const recentImages = (images.length!==0)?images.filter((image)=>{
-    if(image.identifier.indexOf("gallery")!==-1){
-      return image;
-    }else{
-      return null;
-    }
-  }).slice(0,6):[];
-
   return (
-    <div>
-      {recentImages.length !== 0 ? (
-        <div className="w-11/12 mx-auto mt-24 p-6" ref={ref}>
-          <h2 className="colorfont text-3xl mb-6">Recent Gallery</h2>
-          <div className="responsive-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            {recentImages.map((image) => (
-              <div key={image._id} className="relative group overflow-hidden cursor-pointer hover:z-10">
-                <img
-                  src={image.imageLink}
-                  alt="Memory"
-                  className="w-full h-72 object-cover transition-transform transform scale-100 group-hover:scale-110"
-                  onClick={() => handleImageSelect(image)}
-                />
-                <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-60 transition-opacity duration-300 group-hover:opacity-0 flex items-center justify-center">
-                  <p className="text-white text-sm">{formatDate(image.date)}</p>
-                </div>
-              </div>
+    <div className="try " ref={ref}>
+      <h2 className="colorfont ">GALLERY</h2>
+      {images.length !== 0 ? (
+        <div className="responsive-gallery">
+          <div className="left-gallery">
+            {images.slice(0,3).map((image) => (
+              <img
+                key={image.id}
+                src={image.imageLink}
+                alt={image.title}
+                onClick={() => handleImageSelect(image)}
+              />
+            ))}
+          </div>
+          <div className="left-gallery">
+            {images.slice(3,6).map((image) => (
+              <img
+                key={image.id}
+                src={image.imageLink}
+                alt={image.title}
+                onClick={() => handleImageSelect(image)}
+              />
+            ))}
+          </div>
+          <div className="center-gallery">
+            <img
+              src={selectedImage.imageLink || images[7].imageLink}
+              alt={images[7].title}
+              onClick={() => handleImageSelect(null)}
+            />
+          </div>
+          <div className="right-gallery">
+            {images.slice(8,11).map((image) => (
+              <img
+              key={image.id}
+              src={image.imageLink}
+              alt={image.title}
+              onClick={() => handleImageSelect(image)}
+              />
+            ))}
+          </div>
+          <div className="right-gallery">
+            {images.slice(11,14).map((image) => (
+              <img
+              key={image.id}
+              src={image.imageLink}
+              alt={image.title}
+              onClick={() => handleImageSelect(image)}
+              />
             ))}
           </div>
         </div>
@@ -57,7 +132,3 @@ const ResponsiveGallery = forwardRef((props, ref) => {
 });
 
 export default ResponsiveGallery;
-
-
-
-
