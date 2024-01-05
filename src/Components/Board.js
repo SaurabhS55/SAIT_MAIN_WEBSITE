527f91410c21b5744e0d556ab875e35117156d92
TANMAY SHINGDE <tanmayshingde8@gmail.com>
2024-01-05 18:59:17 +0530

    Lazy

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Board = ({ mentor }) => {
   return (
@@ -7,13 +8,12 @@ const Board = ({ mentor }) => {
       <div className="max-w-[300px] min-w-[250px] group relative cursor-pointer overflow-hidden bg-[#0e2032e5] py-6 px-6 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl rounded-lg">
         <span className="absolute top-20 left-24 z-0 h-20 w-20 rounded-full bg-[#183b5ce5] transition-all duration-700 group-hover:scale-[10]"></span>
         <div className="relative z-10 mx-auto max-w-md space-y-3 transition-all group-hover:shadow-lg">
-          <img
-            loading="lazy"
-            placeholder={mentor.Namess}
+          <LazyLoadImage
+            placeholder={mentor.Names}
             src={mentor.Photo}
             alt={mentor.Name}
             className="w-[9rem] h-[9rem] mx-auto rounded-full aspect-square bg-gray-400 transition-all duration-300"
-          ></img>
+          ></LazyLoadImage>
           <div className="space-y-4 text-center divide-y divide-gray-600">
             <div className="my-2 space-y-1 text-white">
               <h2 className="text-xl font-semibold ">{mentor.Name}</h2>
